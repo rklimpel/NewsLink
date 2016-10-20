eeee526bde595b4f83506486cfed1585809105af
Rico Klimpel <rico@tklimpel.de>
2016-10-20 22:39:56 +0200

    First working http request

--- app/src/main/java/de/ricoklimpel/newslink/MainActivity.java
+++ app/src/main/java/de/ricoklimpel/newslink/MainActivity.java
@@ -1,13 +1,100 @@
 package de.ricoklimpel.newslink;
 
-import android.support.v7.app.AppCompatActivity;
+
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView tv_output;
+    String ServerResponse;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        tv_output = (TextView)findViewById(R.id.tv_output);
+
+        // Gets the URL from the UI's text field.
+        String stringUrl = "https://newsapi.org/v1/articles?source=techcrunch&apiKey=bddae599de5041ab9858c74961886e6c";
+        ConnectivityManager connMgr = (ConnectivityManager)
+                getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+        if (networkInfo != null && networkInfo.isConnected()) {
+            new DownloadWebpageTask().execute(stringUrl);
+        } else {
+            tv_output.setText("No network connection available.");
+        }
+
+
     }
+
+    private class DownloadWebpageTask extends AsyncTask<String, Void, String> {
+        @Override
+        protected String doInBackground(String... urls) {
+
+            // params comes from the execute() call: params[0] is the url.
+            try {
+
+                URL url = new URL(urls[0]);
+
+                HttpURLConnection urlConnection = null;
+                try {
+                    urlConnection = (HttpURLConnection) url.openConnection();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                try {
+                    InputStream in = new BufferedInputStream(urlConnection.getInputStream());
+                    ServerResponse = readStream(in);
+                    Log.e("String", ServerResponse);
+                }catch (IOException e) {
+                    e.printStackTrace();
+                }finally {
+                    urlConnection.disconnect();
+                }
+                return ServerResponse;
+
+            }catch (IOException e) {
+                return "Unable to retrieve web page. URL may be invalid.";
+            }
+        }
+        // onPostExecute displays the results of the AsyncTask.
+        @Override
+        protected void onPostExecute(String result) {
+            tv_output.setText(result);
+        }
+    }
+
+
+    private String readStream(InputStream is) {
+        try {
+            ByteArrayOutputStream bo = new ByteArrayOutputStream();
+            int i = is.read();
+            while(i != -1) {
+                bo.write(i);
+                i = is.read();
+            }
+            return bo.toString();
+        } catch (IOException e) {
+            return "";
+        }
+    }
+
+
 }
